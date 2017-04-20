31450336709b366065cca737706255306692bfa2
iamcastelli <sowed@cyberdude.com>
2017-04-20 09:49:22 +0300

    Test menu calls in different script

---
+++ js/renderer.js
@@ -0,0 +1,21 @@
+//document.write('Hello World');
+//alert('Welcome \nTo the new World');
+
+const fs = require('fs'),
+    contents = fs.readFileSync('./package.json', 'utf8');
+//alert(contents);
+
+const remote = require('electron').remote,
+    Menu =  require('electron').menu;
+
+let menu = Menu.buildFromTemplate([{
+    label: 'Startup',
+    submenu: [
+        {
+            label: 'Settings',
+            click: function () {
+                alert('No settings yet');
+            }
+        }
+    ]
+}]);
